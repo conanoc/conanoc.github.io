226132b778cf20fb576c3e8da801844d3648d258
conanoc <conanoc@gmail.com>
2015-08-23 22:00:11 +0900

    add chapters as ebook style viewer

---
+++ book.js
@@ -0,0 +1,85 @@
+var currentIndex = 0;
+
+var content = document.getElementById('content');
+var columnGap = 20;
+var contentMargins = 4*8;
+var pageWidth = window.innerWidth - contentMargins;
+var maxIndex = 0;
+var filename = location.href.substring(location.href.lastIndexOf('/')+1, location.href.lastIndexOf('.'));
+if(content != null)
+  maxIndex = Math.ceil((content.scrollWidth)/ (pageWidth + columnGap)) - 1;
+
+function setPageCount(){
+  var count = document.getElementById("pg-count");
+  count.innerText = (currentIndex+1) + "/" + (maxIndex +1);
+}
+
+function goPage(){
+    content.style.overflow = "auto";
+    content.scrollLeft = (pageWidth + columnGap) * currentIndex  ;
+    content.style.overflow = "hidden";
+    
+    setPageCount();
+    localStorage.chapter = filename;
+    localStorage.page = currentIndex;
+}
+
+function nextPage(){
+  if(currentIndex < maxIndex){
+    currentIndex++;
+    goPage();
+  }
+  else{
+    if(filename == "preface")
+      location.replace("toc.html");
+    else if(filename == "toc")
+      location.replace("ch1.html");
+    else if(filename.substring(0,2) == "ch"){
+      var chapter = parseInt(filename.substring(2));
+      if(chapter < 11)
+        location.replace("ch" + ++chapter + ".html");
+    }
+  }
+}
+
+function prevPage(){
+  if(currentIndex > 0){
+    currentIndex--;
+    goPage();
+  }
+  else{
+    if(filename == "preface")
+      location.replace("cybernetics.html");
+    else if(filename == "toc")
+      location.replace("preface.html");
+    else if(filename.substring(0,2) == "ch"){
+      var chapter = parseInt(filename.substring(2));
+      if(chapter > 1)
+        location.replace("ch" + --chapter + ".html");
+      else if(chapter == 1)
+      location.replace("toc.html");     
+    }
+  }  
+}
+
+function toc(){
+  location.replace("toc.html");
+}
+
+function go(){
+  if(localStorage.chapter == null)
+    localStorage.chapter = "preface";
+    
+  location.replace(localStorage.chapter + ".html");
+}
+
+document.body.onload = function(){
+  if(filename == localStorage.chapter && localStorage.page != undefined){
+    currentIndex = parseInt(localStorage.page);
+    goPage();
+  }
+};
+
+document.body.addEventListener('touchmove', function(event){
+  event.preventDefault();
+});
